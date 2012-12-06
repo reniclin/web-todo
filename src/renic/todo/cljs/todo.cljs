ed57323fe13a3cbd8b2c55238b148868b004bc66
Renic Lin <renic.lin@gmail.com>
2012-12-06 10:44:05 +0800

    add theme toggle button to toggle light/dark theme

--- src/renic/todo/cljs/todo.cljs
+++ src/renic/todo/cljs/todo.cljs
@@ -8,8 +8,9 @@
   ISeqable
   (-seq [array] (array-seq array 0)))
 
-(def todo-personal-key "todo-personal")
-(def todo-work-key "todo-work")
+(def todo-today-key "todo-today")
+(def todo-later-key "todo-later")
+(def theme-key "theme")
 (def storage (.-localStorage js/window))
 
 ;; ========== htmls ==========
@@ -33,7 +34,7 @@
 
 ;; ========== common ==========
 (defn get-saved-items [key]
-  (.log js/console "get-saved-items")
+  ;; (.log js/console "get-saved-items")
   (let [storage-item (.getItem storage key)]
     (if (or (= storage-item nil) (= storage-item ""))
       (array)
@@ -42,20 +43,20 @@
 
 ;; ========== delete ==========
 (defn delete-li [button]
-  (.log js/console "delete-li")
+  ;; (.log js/console "delete-li")
   (.removeChild (.. button -parentNode -parentNode) (.-parentNode button)))
 
 (defn remove-saved [key todo-text]
-  (.log js/console "remove")
-  (.log js/console (str "key: " key))
-  (.log js/console (str "todo-text: " todo-text))
+  ;; (.log js/console "remove")
+  ;; (.log js/console (str "key: " key))
+  ;; (.log js/console (str "todo-text: " todo-text))
   (let [saved-items (get-saved-items key)
         text-index (goog-array/indexOf saved-items todo-text)]
     (.splice saved-items text-index 1)
     (.setItem storage key (.stringify js/JSON saved-items))))
 
 (defn on-delete-click [ev]
-  (.log js/console "on-delete-click")
+  ;; (.log js/console "on-delete-click")
   (this-as button
            (remove-saved
             (.. ev -currentTarget -parentNode -parentNode -id)
@@ -65,7 +66,7 @@
 
 ;; ========== Click list ==========
 (defn mark-done [li]
-  (.log js/console "mark-done")
+  ;; (.log js/console "mark-done")
   (let [icon (icon-delete)
         line (through-line)]
     ;; (.addEventListener icon "click" on-delete-click false)
@@ -75,7 +76,7 @@
   (.setAttribute li "class" "item-done"))
 
 (defn mark-ongoing [li]
-  (.log js/console "mark-ongoing")
+  ;; (.log js/console "mark-ongoing")
   (let [icon-delete (.getElementsByClassName li "icon-delete")
         line (.getElementsByClassName li "through-line")]
     (.removeChild li (.item line 0))
@@ -83,25 +84,25 @@
   (.setAttribute li "class" "item-ongoing"))
 
 (defn toggle-done [li]
-  (.log js/console "toggle-done")
+  ;; (.log js/console "toggle-done")
   (let [li-class (.getAttribute li "class")]
     (if (not= li-class "item-done")
       (mark-done li)
       (mark-ongoing li))))
 
 (defn on-list-click []
-  (.log js/console "on-list-click")
+  ;; (.log js/console "on-list-click")
   (this-as li (toggle-done li)))
 
 
 ;; ========== add ==========
 (defn li-on-dragstart [ev]
-  (.log js/console "li-on-dragstart")
+  ;; (.log js/console "li-on-dragstart")
   (set! (.. ev -dataTransfer -effectAllowed) (array "copy" "move"))
   (.setData (.-dataTransfer ev) "text/plain" (.. ev -target -id)))
 
 (defn add-li [key todo-text]
-  (.log js/console "add-li")
+  ;; (.log js/console "add-li")
   (let [li (.createElement js/document "li")
         ul (.getElementById js/document key)]
     (.setAttribute li "class" "item-ongoing")
@@ -114,20 +115,20 @@
     (.appendChild ul li)))
 
 (defn save [key todo-text]
-  (.log js/console "save")
-  (.log js/console (str "key: " key))
-  (.log js/console (str "todo-text: " todo-text))
+  ;; (.log js/console "save")
+  ;; (.log js/console (str "key: " key))
+  ;; (.log js/console (str "todo-text: " todo-text))
   (let [saved-items (get-saved-items key)]
     (.push saved-items todo-text)
     (.setItem storage key (.stringify js/JSON saved-items))))
 
 (defn add-new-todo [todo-text]
-  (.log js/console "add-new-todo")
-  (add-li todo-personal-key todo-text)
-  (save todo-personal-key todo-text))
+  ;; (.log js/console "add-new-todo")
+  (add-li todo-today-key todo-text)
+  (save todo-today-key todo-text))
 
 (defn ^:export on-add-click []
-  (.log js/console "on-add-click")
+  ;; (.log js/console "on-add-click")
   (let [todo (.getElementById js/document "todo-input")
         todo-text (.-value todo)]
     (if (not= todo-text "") (add-new-todo todo-text) (js/alert "Please input a todo."))
@@ -145,13 +146,13 @@
   (set! (.. ev -dataTransfer -dropEffect) "copy"))
 
 (defn ul-on-drop [ev]
-  (.log js/console "ul-on-drop")
+  ;; (.log js/console "ul-on-drop")
   (.preventDefault ev)
   (.stopPropagation ev)
   (if (> (.. ev -dataTransfer -types -length) 0)
     (doseq [type (.. ev -dataTransfer -types)]
-      (.log js/console (str "types: " (.. ev -dataTransfer -types)))
-      (.log js/console (str "type: " type))
+      ;; (.log js/console (str "types: " (.. ev -dataTransfer -types)))
+      ;; (.log js/console (str "type: " type))
       (if (= type "text/plain")
         (let [source-id (.getData (.-dataTransfer ev) "text/plain")
               source (.getElementById js/document source-id)
@@ -165,12 +166,52 @@
     false))
 
 (defn add-ul-listeners [ul]
-  (.log js/console "add-ul-listeners")
+  ;; (.log js/console "add-ul-listeners")
   (doto ul
     (.addEventListener "dragenter" ul-on-dragenter false)
     (.addEventListener "dragover" ul-on-dragover false)
     (.addEventListener "drop" ul-on-drop false)))
 
+
+;; ========= theme ==========
+
+(defn save-theme [theme]
+  (.setItem storage theme-key theme))
+
+(defn white-light [button]
+  (.setAttribute (.-body js/document) "class" "light")
+  (.setAttribute button "id" "light-select")
+  (save-theme "light")
+  (let [black-button (.getElementById js/document "dark-select")]
+    (if (not= nil black-button)
+      (.setAttribute black-button "id" "dark"))))
+
+(defn black-dark [button]
+  (.setAttribute (.-body js/document) "class" "dark")
+  (.setAttribute button "id" "dark-select")
+  (let [white-button (.getElementById js/document "light-select")]
+    (if (not= nil white-button)
+      (.setAttribute white-button "id" "light")))
+  (save-theme "dark"))
+
+(defn loads-saved-theme []
+  ;; (.log js/console "get-saved-theme")
+  (let [theme (.getItem storage theme-key)]
+    ;; (.log js/console (str "theme: " theme))
+    (if (= theme "dark")
+      (black-dark (.getElementById js/document "dark"))
+      (white-light (.getElementById js/document "light")))))
+
+(defn toggle-theme [button]
+  (let [button-id (.getAttribute button "id")]
+    (cond
+     (= button-id "light") (white-light button)
+     (= button-id "dark") (black-dark button))
+))
+
+(defn ^:export on-toggle-theme [button]
+  ;; (.log js/console "on-toggle-theme")
+  (toggle-theme button))
 
 ;; ========== init ==========
 (defn input-submit [ev]
@@ -179,7 +220,7 @@
   false)
 
 (defn input-key-down [ev]
-  (.log js/console (str "keyCode: " (.-keyCode ev)))
+  ;; (.log js/console (str "keyCode: " (.-keyCode ev)))
   (if (=  13 (.-keyCode ev)) (input-submit ev) false))
 
 (defn init-input []
@@ -190,24 +231,23 @@
       (.focus))))
 
 (defn init-uls []
-  (add-ul-listeners (.getElementById js/document "todo-personal"))
-  (add-ul-listeners (.getElementById js/document "todo-work")))
+  (add-ul-listeners (.getElementById js/document "todo-today"))
+  (add-ul-listeners (.getElementById js/document "todo-later")))
 
 (defn load-todo-list [key]
-  (.log js/console "load-todo-list")
-  (.log js/console (str "key: " key))
+  ;; (.log js/console "load-todo-list")
+  ;; (.log js/console (str "key: " key))
   (let [saved-items (get-saved-items key)]
     (if (not= nil saved-items)
       (doseq [item saved-items]
         (add-li key item)))))
 
 (defn ^:export init []
-  (.log js/console "init")
+  ;; (.log js/console "init")
+  (loads-saved-theme)
   (init-input)
   (init-uls)
-  (load-todo-list todo-personal-key)
-  (load-todo-list todo-work-key))
-
-;; (crate-page/include-css "/css/todo.css")
+  (load-todo-list todo-today-key)
+  (load-todo-list todo-later-key))
 
 (set! (.-onload js/window) init)
